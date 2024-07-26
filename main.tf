91964319f3428a3c3a82c18017d8dd8881779c37
sjpillai <awstrain48@gmail.com>
2024-07-26 16:15:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,15 +24,4 @@ resource "aws_instance" "web-server" {
 
   }
 
-resource "aws_instance" "web-server" {
-
-  ami           = "ami-04a81a99f5ec58529"
-
-  instance_type = "t2.micro"
-
-  key_name      = "nts10key"
-
-
-  tags = {
-
-    "Name": "ec2-nts"
+}
